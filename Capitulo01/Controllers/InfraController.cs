e13b5c73d3345a4d4e2769382e114609c221762e
Tarcísio Ribeiro <tarcisiorib@outlook.com>
2021-04-14 23:21:30 -0300

    Authorization

--- Capitulo01/Controllers/InfraController.cs
+++ Capitulo01/Controllers/InfraController.cs
@@ -34,5 +34,51 @@ namespace Capitulo01.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
+
+        [AllowAnonymous]
+        public IActionResult NovoUsuario(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> NovoUsuario(NovoUsuarioViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new Usuario { UserName = model.Email, Email = model.Email };
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    logger.LogInformation("Usuário criado");
+                    var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    logger.LogInformation("Usuário logado.");
+                    return RedirectToLocal(returnUrl);
+                }
+                AddErrors(result);
+            }
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+            else
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
     }
 }
